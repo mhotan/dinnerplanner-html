fcca470a65a1449737b25ecf0d9f9a9ddc3049a8
Halit Anil Donmez <halitanil.donmez@gmail.com>
2014-02-24 16:10:40 +0100

    done the current menu view and menu controller view

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $(function() {
 	
 	//We instantiate our model
 	var model = new DinnerModel();
-	
+
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"),model);
-   	var exampleViewController = new ExampleViewController(exampleView,model);
+	var exampleView = new CurrentMenuView($("#overallView"),model);
+   	var exampleViewController = new CurrentMenuViewController(exampleView,model);
 });
--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,6 +1,6 @@
 //ExampleView Object constructor
 var ExampleView = function (container,model) {
-	
+
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
